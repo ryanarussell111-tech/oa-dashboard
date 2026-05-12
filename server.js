f769e75570b649201268328f3a2bab4d0252f3d5
ryanarussell111-tech <ryanarussell111@gmail.com>
2026-05-12 19:55:01 -0400

    Update route handling for serving index.html

--- server.js
+++ server.js
@@ -214,5 +214,5 @@ const express = require("express");
 const path = require("path");
 const app = express();
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "build", "index.html")));
+app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname, "build", "index.html")));
 app.listen(process.env.PORT || 4000, () => console.log("Health check server running"));
